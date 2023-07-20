c60a910665ec6495852ab23078bdd6bec3739f9f
Soap <573471669@qq.com>
2023-07-20 14:09:23 +0800

    Create a main function and a score status function.

---
+++ prac_02/score_menu.py
@@ -0,0 +1,37 @@
+MENU = """(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = float(input("Enter score: "))
+        elif choice == "P":
+            print(get_score_status(score))
+        elif choice == "S":
+            for i in range(score):
+                print("*", end="")
+            print()
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Bye,thank you.")
+
+
+def get_score_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
