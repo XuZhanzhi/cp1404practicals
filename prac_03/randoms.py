99fe4921aa031041df0980a48ccc07fd43be98ce
Soap <573471669@qq.com>
2023-07-24 14:57:33 +0800

    Answer 3 questions and produce a random number between 1 and 100 inclusive.

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+# The smallest number is 5, the largest number is 20.
+print(random.randrange(3, 10, 2))  # line 2
+# The smallest number is 3, the largest number is 9.
+print(random.uniform(2.5, 5.5))  # line 3
+# The smallest number is 2.5, the largest number is 5.5.
+
+print(random.randint(0, 100))
