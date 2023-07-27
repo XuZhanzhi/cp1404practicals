44c8d43fb91b2b4e19a8d4c9c3561a3880594b70
Soap <573471669@qq.com>
2023-07-27 13:49:08 +0800

    update new answers.

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -5,6 +5,6 @@ print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 # The smallest number is 3, the largest number is 9.
 print(random.uniform(2.5, 5.5))  # line 3
-# The smallest number is 2.5, the largest number is 5.5.
+# The smallest number is 2.5, the largest number is 5.5(exclusive).
 
 print(random.randint(0, 100))
