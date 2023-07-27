8281f80567213fb2887260306a58f0e14dbb29bb
Soap <573471669@qq.com>
2023-07-27 14:08:41 +0800

    Add days and day prices and edit the output.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -19,10 +19,12 @@ OUTPUT_FILE = "output.txt"
 out_file = open(OUTPUT_FILE, 'w')
 
 price = INITIAL_PRICE
+day = 0
 print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+    day += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -35,6 +37,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {day} price is: ${price:,.2f}", file=out_file)
 
 out_file.close()
