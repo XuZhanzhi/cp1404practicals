36bbbc5735b8ab58e86afae58f58c1c8791fd798
Soap <573471669@qq.com>
2023-07-28 10:07:49 +0800

    finish if any of the 'normal' counts are zero, return False.

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -47,6 +47,8 @@ def is_valid_password(password):
         elif char in SPECIAL_CHARACTERS:
             count_special += 1
     # TODO: if any of the 'normal' counts are zero, return False
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
+        return False
 
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
