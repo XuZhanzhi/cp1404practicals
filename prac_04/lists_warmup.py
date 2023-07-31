bce358c4a6228b9a5bdd99ba0abbb62e9b10e896
Soap <573471669@qq.com>
2023-07-31 15:34:53 +0800

    answer the questions.

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,5 +1,15 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
+# numbers[0] is 3
+# numbers[-1] is 2
+# numbers[3] is 1
+# numbers[:-1] is [3,1,4,1,5,9]
+# numbers[3:4] is [1]
+# 5 in numbers is True
+# 7 in numbers is False
+# "3" in numbers is False
+# numbers + [6, 5, 3] is [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
 # Change the first element of numbers to "ten" (the string, not the number 10)
 numbers[0] = "ten"
 
