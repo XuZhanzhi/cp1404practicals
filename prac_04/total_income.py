c205afcc4bb34d098822f77e759c95aedfb9eafe
Soap <573471669@qq.com>
2023-08-04 12:04:34 +0800

    finish the for loop in the function.

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -17,7 +17,7 @@ def main():
 
 
 def print_income(incomes):
-    """This function is used for printing income report"""
+    """This function is used for printing income report."""
     print("\nIncome Report\n-------------")
     total = 0
     for month, income in enumerate(incomes, 1):
