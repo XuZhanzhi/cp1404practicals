f081121c8dd553c9e949bc8e5d116a585c168718
Soap <573471669@qq.com>
2023-08-04 13:17:36 +0800

    make main function and get the number of quick pick with error checking.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,7 +7,7 @@ NUMBER_PER_LINE = 6
 
 def main():
     """This is the main function for picking numbers and print them"""
-    num_quick_picks = int(input("How many quick picks do you want to generate? "))
+    num_quick_picks = int(input("How many quick picks do you want? "))
     while num_quick_picks < 0:
         print("Please enter a correct number.")
-        num_quick_picks = int(input("How many quick picks do you want to generate? "))
+        num_quick_picks = int(input("How many quick picks do you want? "))
