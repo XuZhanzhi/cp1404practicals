31206c4a240e979b300eb8589988e3680dd5983d
Soap <573471669@qq.com>
2023-08-04 13:27:39 +0800

    finish quick pick number and print them well.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -11,3 +11,16 @@ def main():
     while num_quick_picks < 0:
         print("Please enter a correct number.")
         num_quick_picks = int(input("How many quick picks do you want? "))
+
+    for i in range(num_quick_picks):
+        quick_pick_number = []
+        for j in range(NUMBER_PER_LINE):
+            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+            while number in quick_pick_number:
+                number = number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+            quick_pick_number.append(number)
+        quick_pick_number.sort()
+        print(" ".join(f"{number:2}" for number in quick_pick_number))
+
+
+main()
