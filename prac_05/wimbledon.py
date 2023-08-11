671b5b71584d91d85cb2d46749889bb756503638
Soap <573471669@qq.com>
2023-08-11 15:34:30 +0800

    Creating function for creating dictionary of champions and set of countries.

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,4 +1,6 @@
 FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
 
 
 def get_records(filename):
@@ -9,5 +11,19 @@ def get_records(filename):
             parts = line.strip().split(",")
             records.append(parts)
     return records
+
+
+def process_data(data):
+    """This function is for creating dictionary of champions and set of countries"""
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[COUNTRY_INDEX])
+        try:
+            champion_to_count[record[CHAMPION_INDEX]] += 1
+        except KeyError:
+            champion_to_count[record[CHAMPION_INDEX]] = 1
+    return champion_to_count, countries
+
 
 
