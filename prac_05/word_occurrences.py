aa095a1138f75faa8655e2443004c7f21414062e
Soap <573471669@qq.com>
2023-08-11 14:47:43 +0800

    align the outputs so the numbers are in one nice column.

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,5 +8,6 @@ for word in words:
 words = list(word_to_count.keys())
 words.sort()
 
+max_length = max((len(word) for word in words))
 for word in words:
-    print(f"{word} : {word_to_count[word]}")
+    print(f"{word:{max_length}} : {word_to_count[word]}")
