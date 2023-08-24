53c8c09dd3684888a3c1264ced88e769ecc893c8
Soap <573471669@qq.com>
2023-08-24 15:42:18 +0800

    The sample output uses some nice string formatting.

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -15,5 +15,18 @@ def main():
         print(guitar_to_add, "added.")
         name = input("Name: ")
 
+        guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+        guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+        if guitars:
+            print("These are my guitars:")
+            for i, guitar in enumerate(guitars, 1):
+                vintage_string = ""
+                if guitar.is_vintage():
+                    vintage_string = "(vintage)"
+                print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+        else:
+            print("Go and buy a new guitar!")
+
 
 main()
