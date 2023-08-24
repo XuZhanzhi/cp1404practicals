68199c2495eb5c480850cf67f4620cd68e7af614
Soap <573471669@qq.com>
2023-08-24 15:25:40 +0800

    Collect guitar information.

---
+++ prac_06/guitars.py
@@ -0,0 +1,19 @@
+from prac_06.guitar import Guitar
+
+
+def main():
+    """Main program for using a list to store all the user's guitars."""
+    guitars = []
+
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
+
+
+main()
