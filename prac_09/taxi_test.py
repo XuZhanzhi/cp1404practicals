06e0bec600599c2e67542b3c4986c7e016e6b88f
Soap <573471669@qq.com>
2023-09-15 14:25:47 +0800

    Write lines of code for the following requests.

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,24 @@
+from taxi import Taxi
+
+
+def main():
+    # Create a new taxi object
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+
+    # Drive the taxi 40 km
+    my_taxi.drive(40)
+
+    # Print the taxi's details and the current fare
+    print(my_taxi)
+    print("Current fare: ${:.2f}".format(my_taxi.get_fare()))
+
+    # Restart the meter (start a new fare) and then drive the car 100 km
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+
+    # Print the details and the current fare
+    print(my_taxi)
+    print("Current fare: ${:.2f}".format(my_taxi.get_fare()))
+
+
+main()
